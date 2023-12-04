8fefc07f402aae8eee4463d5b7e780d11ea86f07
Miguel Vizcaino <miguel87831@gmail.com>
2023-12-03 22:59:14 -0500

    ignore typescript in build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    reactStrictMode: false,
     images: {
       remotePatterns: [
         {
@@ -15,6 +16,12 @@ const nextConfig = {
         }
       ],
     },
+    experimental: {
+        appDir: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
   }
   
   module.exports = nextConfig
